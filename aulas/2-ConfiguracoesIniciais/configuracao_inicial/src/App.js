1557bdebc05d835b0f3128783020c1670522ce75
ICEBERG776 <henriqueau111@gmail.com>
2023-01-15 14:51:02 -0300

    new file:   aulas/2-ConfiguracoesIniciais/configuracao_inicial/.gitignore 	new file:   aulas/2-ConfiguracoesIniciais/configuracao_inicial/README.md 	new file:   aulas/2-ConfiguracoesIniciais/configuracao_inicial/package-lock.json 	new file:   aulas/2-ConfiguracoesIniciais/configuracao_inicial/package.json 	new file:   aulas/2-ConfiguracoesIniciais/configuracao_inicial/public/index.html 	new file:   aulas/2-ConfiguracoesIniciais/configuracao_inicial/src/App.css 	new file:   aulas/2-ConfiguracoesIniciais/configuracao_inicial/src/App.js 	new file:   aulas/2-ConfiguracoesIniciais/configuracao_inicial/src/index.js

---
+++ aulas/2-ConfiguracoesIniciais/configuracao_inicial/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './App.css';
+
+function App() {
+  
+  function hello() {
+   const teste = <h1>Teste 1 2 3</h1>
+
+    return teste;
+
+    
+  }
+
+  return hello()  
+};
+
+export default App;
